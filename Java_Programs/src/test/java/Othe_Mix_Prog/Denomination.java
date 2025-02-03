c3a6a2561a7205f63dc6b91339b0a8e69acdd6c5
OM <OM@DESKTOP-0BC3S1D>
2025-02-03 16:27:03 +0530

    adding few program

--- Java_Programs/src/test/java/Othe_Mix_Prog/Denomination.java
+++ Java_Programs/src/test/java/Othe_Mix_Prog/Denomination.java
@@ -12,10 +12,10 @@ public class Denomination {
 		public void denomination() {
 		List<Integer> denominations=new ArrayList<Integer>(Arrays.asList(2000,1000,500,200,100,50,20,10,5,2,1));
 		LinkedHashMap<Integer, Integer> map=new LinkedHashMap<Integer, Integer>();
-		
+		 
 		int amount=8825;
 		for (Integer denomination : denominations) {
-			if (denomination<=amount) {
+			if (amount>=denomination) {
 				
 				int count=amount/denomination;
 				amount%=denomination;
--- Java_Programs/src/test/java/String_Java_Pragrams/CompareTwoOccrance_InString.java
+++ Java_Programs/src/test/java/String_Java_Pragrams/CompareTwoOccrance_InString.java
@@ -1,50 +1,107 @@
 package String_Java_Pragrams;
 
-
-
-	import java.util.LinkedHashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.testng.annotations.Test;
 
-	public class CompareTwoOccrance_InString {
-		@Test
-		public void test() {
-			String name = "hellooo welcomeeehh";
-			String[] s = name.split(" ");
-			String str = s[0], str2 = s[1];
-			LinkedHashMap<Character, Integer> map = new LinkedHashMap<Character, Integer>();
-			for (int i = 0; i < str.length(); i++) {
-				if (map.containsKey(str.charAt(i))) {
-					map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
-				} else {
-					map.put(str.charAt(i), 1);
-				}
-			}
+public class CompareTwoOccrance_InString {
+	@Test
+	public void test() {
 
-			LinkedHashMap<Character, Integer> map2 = new LinkedHashMap<Character, Integer>();
-			for (int i = 0; i < str2.length(); i++) {
-				if (map2.containsKey(str2.charAt(i))) {
-					map2.put(str2.charAt(i), map2.get(str2.charAt(i)) + 1);
-				} else {
-					map2.put(str2.charAt(i), 1);
-				}
-			}
+		// import java.util.*;
+		String s = "aabbcccccccccccc";
+		String s1 = "aaabbbcccc";
 
-			//System.out.println(map + "\n" + map2);
-			LinkedHashMap<Character, Integer> result = new LinkedHashMap();
-			for(char key : map.keySet()) {
-				int value1 = map.get(key);
-				int value2 = map2.getOrDefault(key, 0);
-				result.put(key, Math.max(value1, value2));
-			}
-			
-			for(char key : map2.keySet()) {
-				if(!result.containsKey(key)) {
-					result.put(key, map2.get(key));
-				}
-			}
-			System.out.println(result);
+		// output:=====>> {a=3, b=3, c=12}
 
+		LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+		LinkedHashMap<Character, Integer> map2 = new LinkedHashMap<>();
+
+		// Count occurrences of characters in s
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if (map.containsKey(ch)) {
+				map.put(ch, map.get(ch) + 1);
+			} else {
+				map.put(ch, 1);
+			}
 		}
+		// Count occurrences of characters in s1
+		for (int i = 0; i < s1.length(); i++) {
+			char ch2 = s1.charAt(i);
+			if (map2.containsKey(ch2)) {
+				map2.put(ch2, map2.get(ch2) + 1);
+			} else {
+				map2.put(ch2, 1);
+			}
+		}
+
+		// Store the maximum count of each character in result map
+		LinkedHashMap<Character, Integer> result = new LinkedHashMap<>();
+		for (char key : map.keySet()) {
+			int value1 = map.get(key);
+			int value2 = map2.get(key);
+			result.put(key, Math.max(value1, value2));
+		}
+		for (char key : map2.keySet()) {
+			if (!result.containsKey(key)) {
+				result.put(key, map2.get(key));
+			}
+		}
+		System.out.println(result);
 	}
 	
+	// @nd  CompareTwoOccrance_Print_Highest_Character
+	@Test
+	public void CompareTwoOccrance_Print_Highest_Char() {
+		
+		        String s = "aabbcccccccccccc";
+		        String s1 = "aaabbbcccc";
+		        
+		        // output :======>>>> Character counts: {a=3, b=3, c=12}
+		       //                     Highest occurring character: c (12 times)
+		        
+		        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+		        LinkedHashMap<Character, Integer> map2 = new LinkedHashMap<>();
+
+		        // Count occurrences of characters in s
+		        for (char ch : s.toCharArray()) {
+		            map.put(ch, map.getOrDefault(ch, 0) + 1);
+		        }
+
+		        // Count occurrences of characters in s1
+		        for (char ch2 : s1.toCharArray()) {
+		            map2.put(ch2, map2.getOrDefault(ch2, 0) + 1);
+		        }
+
+		        // Store the maximum count of each character in result map
+		        LinkedHashMap<Character, Integer> result = new LinkedHashMap<>();
+		        for (char key : map.keySet()) {
+		            int value1 = map.get(key);
+		            int value2 = map2.getOrDefault(key, 0);  // Avoid NullPointerException
+		            result.put(key, Math.max(value1, value2));
+		        }
+		        
+		        // Add remaining characters from map2 that are not in result
+		        for (char key : map2.keySet()) {
+		            result.putIfAbsent(key, map2.get(key));
+		        }
+
+		        // Find the character with the highest occurrence
+		        char highestChar = '\0';
+		        int maxCount = 0;
+
+		        for (Map.Entry<Character, Integer> entry : result.entrySet()) {
+		            if (entry.getValue() > maxCount) {
+		                maxCount = entry.getValue();
+		                highestChar = entry.getKey();
+		            }
+		        }
+
+		        System.out.println("Character counts: " + result);
+		        System.out.println("Highest occurring character: " + highestChar + " (" + maxCount + " times)");
+
+
+	}
+}
